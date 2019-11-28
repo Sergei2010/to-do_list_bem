f51e2fd22b9e785e0d3cd940bde651d777a3f434
sergei2010 <sergejmatvejcuk323@gmail.com>
2019-11-28 22:17:40 +0200

    необходимо переписать проект с учетом требований BEM

---
+++ script/script.js
@@ -0,0 +1,41 @@
+$(function () {
+
+    $('.hide').on('click', function () {
+            $(this).parent('.to_do_open').css('display', 'none');
+            $(this).parent('.to_do_close').css('display', 'none');
+            if(($('.hide').is(':visible'))) {
+            console.clear();
+            console.log('Ещё есть видимые элементы!');
+            }
+            else if(($(this).is(':hidden'))) {
+                    $('#to_do_list').append($('<article class="list_is_empty"><p class="text_to_do">' +
+                    'Список' + ' пуст' + ' ...</p></article>')).css('display', 'block');
+            console.clear();
+            console.log('Теперь нет видимых элементов!');
+
+            }
+    });
+
+    $(".open").on('click', function () {
+        if ($(this).parent('.to_do_open').attr('class') === 'to_do_open') {
+            $(this).attr('src', 'style/img/close_icon.png');
+            $(this).parent('.to_do_open').attr('class', 'to_do_close');
+        } else if ($(this).parent('.to_do_close').attr('class') === 'to_do_close') {
+            $(this).attr('src', 'style/img/open_icon.png');
+            $(this).parent('.to_do_close').attr('class', 'to_do_open');
+        }
+    });
+
+    $('#add_to_do').on('click', function () {
+        $('#to_do_next').attr('id','');
+        $("#to_do_1")
+            .clone(true)
+            .appendTo("#to_do_list")
+            .attr({id: 'to_do_next', class: 'to_do_open'})
+            .css('display', 'block');
+        $('#to_do_next h3').text($('#name_field').val());
+        $('#to_do_next p').text($('#desc_field').val());
+        $('.to_do_new textarea').val('');
+        $('.list_is_empty').remove();
+    });
+});
